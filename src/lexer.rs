909fa375c4637edb6bc9748c3046817d4c22b289
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-06-07 18:49:44 +0300

    lexer

---
+++ src/lexer.rs
@@ -0,0 +1,69 @@
+use regex::Regex;
+
+pub struct Lexer<T : Clone> {
+    rules: Vec<(Regex, T)>,
+    input: String,
+    position: usize
+}
+
+impl<T : Clone> Lexer<T> {
+    pub fn new(input_rules: Vec<(&str, T)>, input: String) -> Result<Self, String> {
+        Ok(Lexer {
+            rules: input_rules.iter().map(|x| (Regex::new(x.0).unwrap(), x.1.clone())).collect(), 
+            input: input.clone(),
+            position: 0
+        })
+    } 
+
+    pub fn skip_whitespace(&mut self) {
+        let mut i = self.input[self.position..].chars();
+
+        while let Some(ch) = i.next()     {
+            match ch {
+                ch if ch.is_whitespace() => {
+                    self.position += 1;
+                    continue
+                },
+                _ => break
+            }
+        }
+    }
+
+    pub fn next_token(&mut self) -> Result<Option<(T, String)>, (String, String)> {
+        self.skip_whitespace();
+
+        if self.position >= self.input.len() {
+            return Ok(None);
+        }
+
+        let remaining_input = &self.input[self.position..];
+
+        for (regex, token_type) in &self.rules {
+            if let Some(mat) = regex.find(&remaining_input) {
+                if mat.start() == 0 {
+                    self.position += mat.end();
+
+                    return Ok(Some((token_type.clone(), mat.as_str().to_string())));
+                }
+            }
+        }
+
+        return Err(("Unknown token".to_string(), remaining_input.to_string()));
+    }
+
+    pub fn lex(&mut self) -> Result<Vec<(T, String)>, (String, String)> {
+        let mut tokens: Vec<(T, String)> = vec![];
+
+        loop {
+            let t = self.next_token();
+
+            match t {
+                Ok(Some(token)) => tokens.push(token),  // Token
+                Ok(None) => break,                      // End of file
+                Err(error) => return Err(error)         // Unknown token
+            }
+        }
+        
+        return Ok(tokens);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::error::Error;
+
+mod lexer;
+use lexer::Lexer;
+
+#[derive(Debug, Clone)]
+enum TokenType {
+    Word,
+
+    NumberLiteral,
+    StringLiteral,
+    
+    Plus,
+    
+    Equal,
+    Less,
+    More,
+
+    Comma,
+    
+    Minus,
+    OpenParan,
+    ClosenParan,
+
+    OpenBracket,
+    ClosenBracket,
+
+    Semicolon
 }
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let rules = vec![
+        (r"[A-Za-z]+",              TokenType::Word),
+        (r"[0-9]+",                 TokenType::NumberLiteral),
+        ("\"([^\"\\\\]|\\\\.)*\"",  TokenType::StringLiteral),
+        (r"\+",                     TokenType::Plus),
+        (r"\=",                     TokenType::Equal),
+        
+        (r"<",                      TokenType::Less),
+        (r">",                      TokenType::More),
+        
+        (r"\-",                     TokenType::Minus),
+        (r"\(",                     TokenType::OpenParan),
+        (r"\)",                     TokenType::ClosenParan),
+        
+        (r"\;",                     TokenType::Semicolon),
+        (r"\,",                     TokenType::Comma),
+        (r"\{",                     TokenType::OpenBracket),
+        (r"\}",                     TokenType::ClosenBracket),
+    ];
+
+    let input: String = fs::read_to_string("test.txt")?;
+    let tokens = Lexer::new(rules, input)?.lex();
+
+    println!("{:?}", tokens);
+
+    Ok(())
+}
+
