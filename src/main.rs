9e05ffca047ff2b63981e892b941c099ab708de3
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-06-14 20:56:44 +0300

    more recursion

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,6 @@ enum TokenType {
     VariableLiteral,
     
     Plus,
-    // Multiplication,
-    // Minus,
-
     Equal,
     
     OpenParan,
@@ -22,50 +19,32 @@ enum TokenType {
     Semicolon
 }
 
-struct LLParser {
+struct Parser {
     head: usize
 }
 
 type Result<T> = result::Result<T, ()>;
 
-impl LLParser {
+impl Parser {
     fn new() -> Self {
-        LLParser {
+        Parser {
             head: 0
         }
     }
 
     fn parse(&mut self, tokens: &Vec<(TokenType, String)>, grammar: &Vec<Production<TokenType>>, entry: &GrammaSymbols<TokenType>) -> Result<()> {
-        println!("-> {:?}", entry);
-
         match entry {
             GrammaSymbols::<TokenType>::NonTerminal(_) => {
                 for prod in grammar.iter() {
-                    println!("prod -> {:?} {:?}", entry.clone(), prod.left);
-
                     if prod.left != entry.clone() { continue; }
-                    
-                    println!("expand prod");
 
                     let backup: usize = self.head;
 
                     let mut suc: bool = false;
 
-                    for right in prod.right.0.iter() {
-                        if self.parse(tokens, grammar, &right.clone()).is_err() {
-                            self.head = backup;
-                            println!("Broke !");
-                            suc = false;
-                            break;
-                        } else {
-                            suc = true;
-                        }
-                    }
-                    
-                    if !suc {
-                        let backup: usize = self.head;
-                        for right in prod.right.1.iter() {
-                            if self.parse(tokens, grammar, &right.clone()).is_err() {
+                    for right in prod.right.iter() {
+                        for symbol in right.iter() {
+                            if self.parse(tokens, grammar, &symbol.clone()).is_err() {
                                 self.head = backup;
                                 suc = false;
                                 break;
@@ -73,25 +52,21 @@ impl LLParser {
                                 suc = true;
                             }
                         }
-                    } 
-                   
-                    if suc {
-                        return Ok(());
                     }
-                    
-                    return Err(());
+
+                    return match suc {
+                        true => Ok(()),
+                        false => Err(()),
+                    }
                 }
 
                 return Err(())
             },
             GrammaSymbols::<TokenType>::Terminal(token) => {
-                println!("token {:?}", token);
-                
                 if self.head >= tokens.len() {
                     return Err(())
                 }
 
-                println!("token {:?} {:?}", token, tokens[self.head].0);
                 if token.clone() == tokens[self.head].0 {
                     self.head += 1;
                     return Ok(());
@@ -193,138 +168,11 @@ impl LLParser {
      */
 }
 
-/*
-type ParseResult<T> = Result<(T, String), String>;
-
-trait Parser {
-    fn parse(&self, slice: &[TokenType]) -> Result<(), String>;
-}
-
-
-struct OrParser;
-
-impl Parser for OrParser {
-    fn parse(&self, slice: &[TokenType]) -> Result<(), String> {
-        todo!()
-    }
-}
-
-struct TokenParser(TokenType);
-
-impl Parser for TokenParser {
-    fn parse(&self, slice: &[TokenType]) -> Result<(), String> {
-        todo!()
-    }
-}
-
-struct ItemsSet(Vec<Production<TokenType>>);
- */
-
-// Todo check for depth recursion
-fn remove_left_recursion(grammar: Vec<Production<TokenType>>) -> Vec<Production<TokenType>> {
-    let mut new: Vec<Production<TokenType>> = vec![];
-
-    for prod in grammar.iter() {
-        if prod.left != prod.right.0[0] {
-            new.push(prod.clone());
-            continue;
-        }
-        
-        let mut a_star_name: String = prod.left.get_string().to_string();
-        a_star_name.push('\'');
-
-        let a_start_non_term =  GrammaSymbols::NonTerminal(a_star_name.clone());
-
-        let mut a_star_right = Vec::from(&prod.right.0[1..prod.right.0.len()]);
-        a_star_right.push(a_start_non_term.clone());  
-
-        let mut a_right = prod.right.1.clone();
-        a_right.push(a_start_non_term);
-
-        new.push(Production {
-            left: prod.left.clone(),
-            right: ( a_right , vec![] )
-        });
-        
-        new.push(Production {
-            left: GrammaSymbols::NonTerminal(a_star_name),
-            right: ( a_star_right, vec![ GrammaSymbols::Sigma ] )
-        });
-    }
-
-    new
-}
-
-fn print_grammar(grammar: &Vec<Production<TokenType>>) {
-    for prod in grammar.iter() {
-        print!("{:?} => ", prod.left);
-
-        print!("{:?}, ", prod.right.0);
-
-        print!(" | ");
-
-        print!("{:?}, ", prod.right.1);
-
-        print!("\n");
-    }
-}
-
 fn main() {
-    // expr -> term = term ;
-    // term -> term + num | num 
-
-
-    let grammar0 = vec![
-        Production {
-            left: sym!("expr"),
-            right: ( 
-                vec![ sym!("term"), sym!(TokenType::Equal), sym!("term"), sym!(TokenType::Semicolon) ],
-                vec![ ]
-            )
-        },
-        Production {
-            left: sym!("term"),
-            right: ( 
-                vec![ sym!("term"), sym!(TokenType::Plus), sym!("num") ], 
-                vec![ sym!("num") ] 
-            )
-        },
-        Production {
-            left: sym!("num"),
-            right: ( 
-                vec![ sym!(TokenType::VariableLiteral) ], 
-                vec![ ] 
-            )
-        }
-    ];
-    /*
-    let grammar0 = vec![
-        Production {
-            left: sym!("expr"),
-            right: ( 
-                vec![ sym!("expr"), sym!(TokenType::Plus), sym!("num"), ],
-                vec![ sym!("num") ]
-            )
-        },
-        Production {
-            left: sym!("num"),
-            right: ( 
-                vec![ sym!(TokenType::VariableLiteral) ], 
-                vec![] 
-            )
-        }
-    ];
-     */
-
-    println!("Grammar 0");
-    print_grammar(&grammar0);
-    println!("");
-
     let rules = vec![
         (r"[A-Za-z]+",              TokenType::VariableLiteral),
-        (r"\+",                     TokenType::Plus),
-        // (r"\-",                     TokenType::Minus),
 
+        (r"\+",                     TokenType::Plus),
         (r"\=",                     TokenType::Equal),
 
         (r"\(",                     TokenType::OpenParan),
@@ -332,117 +180,20 @@ fn main() {
         (r"\;",                     TokenType::Semicolon),
     ];
 
-    
+    let grammar: Grammar<TokenType> = vec![
+        prod!("expr" => ("term", TokenType::Equal, "term")),
+        prod!("term" => ("term", TokenType::Plus, "num") | ("num")),
+        prod!("num" => (TokenType::VariableLiteral)),
+    ].remove_left_recursion();
 
+    grammar.debug_log();
 
-    // let g0 = make_productions![
-    //     ("expression"  => ("term", TokenType::Equal, "term")),
-    //     ("term"        => ("term_prefix", "term_sufix")),
-    //     ("term_sufix"  => (TokenType::Plus, "term_prefix", "term_sufix")),
-    //     ("term_prefix" => (TokenType::OpenParan, "term", TokenType::ClosenParan))
-    // ];
-
-    // expr -> expr + num | num
-    // num -> 'x' | 'y' 
-
-    let grammar1 = remove_left_recursion(grammar0);
-
-    println!("Grammar 1");
-    print_grammar(&grammar1);
-    println!("");
-
-    let tokens: Vec<(TokenType, String)> = Lexer::new(rules, "x + x = x + x + x;".to_string()).unwrap().lex().unwrap();
-
-    println!("{:?}", tokens);
-
-    println!("{:?}", LLParser::new().parse(&tokens, &grammar1, &sym!("expr")));    
-
-
-    /*
-    let rules = vec![
-        (r"[A-Za-z]+",              TokenType::VariableLiteral),
-        (r"\+",                     TokenType::Plus),
-        // (r"\-",                     TokenType::Minus),
-
-        (r"\=",                     TokenType::Equal),
-
-        (r"\(",                     TokenType::OpenParan),
-        (r"\)",                     TokenType::ClosenParan),
-        (r"\;",                     TokenType::Semicolon),
-    ];
-
-    let tokens: Vec<(TokenType, String)> = Lexer::new(rules, "()".to_string()).unwrap().lex().unwrap().into_iter().map(|x| x.0).collect();
-    // println!("{:?}", tokens);
-
-    let s: TokenParser = TokenParser(TokenType::OpenParan);
-
-    println!("{:?}", s.parse(&tokens));
-     */
-    /*
-        let rules = vec![
-        (r"[A-Za-z]+",              TokenType::VariableLiteral),
-        (r"\+",                     TokenType::Plus),
-        // (r"\-",                     TokenType::Minus),
-
-        (r"\=",                     TokenType::Equal),
-
-        (r"\(",                     TokenType::OpenParan),
-        (r"\)",                     TokenType::ClosenParan),
-        (r"\;",                     TokenType::Semicolon),
-    ];
-
-    let grammar = vec![
-        Production {
-            left: sym!("expression"),
-            right: vec![ 
-                vec![ sym!("term"), sym!(TokenType::Equal), sym!("term"), ]
-            ]
-        },
-        Production {
-            left: sym!("term"),
-            right: vec![ 
-                vec![ sym!("term_prefix"), sym!("term_sufix"), ]
-            ]
-        },
-        Production {
-            left: sym!("term_sufix"),
-            right: vec![ 
-                vec![ sym!(TokenType::Plus), sym!("term_prefix"), sym!("term_sufix") ],
-                vec![ sigma!() ]
-            ]
-        },
-        Production {
-            left: sym!("term_prefix"),
-            right: vec![ 
-                vec![ sym!(TokenType::OpenParan), sym!("term"), sym!(TokenType::ClosenParan) ],
-                vec![ sym!(TokenType::VariableLiteral)]
-            ]
-        }
-    ];
-
-    println!("{:?}\n", grammar);
-
-    // let g0 = make_productions![
-    //     ("expression"  => ("term", TokenType::Equal, "term")),
-    //     ("term"        => ("term_prefix", "term_sufix")),
-    //     ("term_sufix"  => (TokenType::Plus, "term_prefix", "term_sufix")),
-    //     ("term_prefix" => (TokenType::OpenParan, "term", TokenType::ClosenParan))
-    // ];
-
-
-    // // # No left recursion
-    // // <expressions> ::= <expression> ';'
-    // // <expression> ::= <term> '=' <term>
-    // // <term> ::= <term_prefix> <term_sufix>
-    // // <term_sufix> ::= '+' <term_prefix> <term_sufix> | ε
-    // // <term_prefix> ::= '(' <term> ')' | 'x'| 'y'
-// 
-    // let input: String = fs::read_to_string("test.txt").unwrap();
-// 
-    let tokens: Vec<(TokenType, String)> = Lexer::new(rules, "((x+(x+y))+(x+y)+(x+y+x))+x+((x+(x+y))+(x+y)+(x+y+x))=((x+y)+(x+y)+(x+y+x))".to_string()).unwrap().lex().unwrap();
-    println!("{:?}", tokens);
-// 
-    println!("{:?}", LLParser::new().parse_expression(&tokens));    
-    */
+    let tokens: Vec<(TokenType, String)> = 
+        Lexer::new(rules, "x + x = x + x + x;".to_string())
+        .unwrap()
+        .lex()
+        .unwrap();
+   
+    println!("{:?}", Parser::new().parse(&tokens, &grammar, &sym!("expr")));    
 }
 
