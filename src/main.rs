d1dd1203480c9f7dc2cdf198ed219466b03fc556
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-06-15 17:03:16 +0300

    updated some code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use std::result;
-
 mod lexer;
 use lexer::Lexer;
-
-#[macro_use]
-mod bnf;
-use bnf::*;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum TokenType {
@@ -19,154 +13,7 @@ enum TokenType {
     Semicolon
 }
 
-struct Parser {
-    head: usize
-}
-
-type Result<T> = result::Result<T, ()>;
-
-impl Parser {
-    fn new() -> Self {
-        Parser {
-            head: 0
-        }
-    }
-
-    fn parse(&mut self, tokens: &Vec<(TokenType, String)>, grammar: &Vec<Production<TokenType>>, entry: &GrammaSymbols<TokenType>) -> Result<()> {
-        match entry {
-            GrammaSymbols::<TokenType>::NonTerminal(_) => {
-                for prod in grammar.iter() {
-                    if prod.left != entry.clone() { continue; }
-
-                    let backup: usize = self.head;
-
-                    let mut suc: bool = false;
-
-                    for right in prod.right.iter() {
-                        for symbol in right.iter() {
-                            if self.parse(tokens, grammar, &symbol.clone()).is_err() {
-                                self.head = backup;
-                                suc = false;
-                                break;
-                            } else {
-                                suc = true;
-                            }
-                        }
-                    }
-
-                    return match suc {
-                        true => Ok(()),
-                        false => Err(()),
-                    }
-                }
-
-                return Err(())
-            },
-            GrammaSymbols::<TokenType>::Terminal(token) => {
-                if self.head >= tokens.len() {
-                    return Err(())
-                }
-
-                if token.clone() == tokens[self.head].0 {
-                    self.head += 1;
-                    return Ok(());
-                }
-        
-                return Err(()); 
-            },
-            sigma!() => {
-                return Ok(());
-            }
-        }
-    }
-
-    /*
-    fn sigma(&mut self, tokens: &Vec<(TokenType, String)>) -> Result<()> {
-        return Ok(());
-    }
-
-    fn accept(&mut self, tokens: &Vec<(TokenType, String)>, symbol: char) -> Result<()> {
-        if self.head >= tokens.len() {
-            return Err(())
-        }
-
-        let cur: char = tokens[self.head].1.chars().next().unwrap();
-
-        if cur == symbol {
-            self.head += 1;
-            return Ok(());
-        }
-
-        Err(())
-    }
-
-    fn parse_term_sufix(&mut self, tokens: &Vec<(TokenType, String)>) -> Result<()> { 
-        let backup: usize = self.head;
-
-        if self.accept(tokens, '+').is_ok() && self.parse_term_prefix(tokens).is_ok() && self.parse_term_sufix(tokens).is_ok() {
-            return Ok(());
-        } else {
-            self.head = backup;
-        }
-
-        if self.sigma(tokens).is_ok() {
-            return Ok(());
-        } else {
-            self.head = backup;
-        }
-
-        Err(())
-    }
-
-    fn parse_term_prefix(&mut self, tokens: &Vec<(TokenType, String)>) -> Result<()> {
-        let backup: usize = self.head;
-
-        if self.accept(tokens, '(').is_ok() && self.parse_term(tokens).is_ok() && self.accept(tokens, ')').is_ok() {
-            return Ok(());
-        } else {
-            self.head = backup;
-        }
-
-        if self.accept(tokens, 'x').is_ok() {
-            return Ok(());
-        } else {
-            self.head = backup;
-        }
-        
-        if self.accept(tokens, 'y').is_ok() {
-            return Ok(());
-        } else {
-            self.head = backup;
-        }
-
-        Err(())
-    }
-
-    fn parse_term(&mut self, tokens: &Vec<(TokenType, String)>) -> Result<()> {
-        let backup: usize = self.head;
-
-        if self.parse_term_prefix(tokens).is_ok() && self.parse_term_sufix(tokens).is_ok() {
-            return Ok(())
-        } else {
-            self.head = backup;
-        }
-
-        Err(())
-    }
-
-    fn parse_expression(&mut self, tokens: &Vec<(TokenType, String)>) -> Result<()> {
-        let backup: usize = self.head;
-
-        if self.parse_term(tokens).is_ok() && self.accept(tokens, '=').is_ok() && self.parse_term(tokens).is_ok() {
-            return Ok(())
-        } else {
-            self.head = backup;
-        }
-
-        Err(())
-    }
-     */
-}
+use uroboros::{gram, grammar::*, parser::{ll_grammar_parser::*, Parser}, sym};
 
 fn main() {
     let rules = vec![
@@ -180,20 +27,27 @@ fn main() {
         (r"\;",                     TokenType::Semicolon),
     ];
 
-    let grammar: Grammar<TokenType> = vec![
-        prod!("expr" => ("term", TokenType::Equal, "term")),
-        prod!("term" => ("term", TokenType::Plus, "num") | ("num")),
-        prod!("num" => (TokenType::VariableLiteral)),
+    let grammar: Grammar<TokenType> = gram![
+        ("expr" => ("term", TokenType::Equal, "term")),
+        ("term" => ("term", TokenType::Plus, "num") | ("num")),
+        ("num" => (TokenType::VariableLiteral))
     ].remove_left_recursion();
 
     grammar.debug_log();
 
     let tokens: Vec<(TokenType, String)> = 
-        Lexer::new(rules, "x + x = x + x + x;".to_string())
+        Lexer::new(rules, "x = x;".to_string())
         .unwrap()
         .lex()
         .unwrap();
    
-    println!("{:?}", Parser::new().parse(&tokens, &grammar, &sym!("expr")));    
+    let result = LLGrammarParser::new(grammar, sym!("expr")).parse(&tokens);
+
+    println!("{:?}", result);
+
+    match result.unwrap() {
+        Some(tree) => (*tree).debug_log(),
+        None => todo!(),
+    }    
 }
 
