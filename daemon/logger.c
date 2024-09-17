012d9cd8348b1d141dc54eb13fa0e2f1cc2d75d3
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-17 19:52:31 +0300

    nice logger colors

--- daemon/logger.c
+++ daemon/logger.c
@@ -1,10 +1,20 @@
 #include "logger.h"
 
-char* stringify_log_level(LogLevel logLevel) {
+const char* unix_log_level_color(LogLevel logLevel) {
     switch (logLevel) {
-        case INFO:       { return "INFO"; }
-        case WARNING:    { return "WARNING"; }
-        case ERROR:      { return "ERROR"; }
+        case INFO:       { return KGRN; }
+        case WARNING:    { return KYEL; }
+        case ERROR:      { return KRED; }
+    }
+
+    return NULL;
+}
+
+const char* stringify_log_level(LogLevel logLevel) {
+    switch (logLevel) {
+        case INFO:       { return "INF"; }
+        case WARNING:    { return "WAR"; }
+        case ERROR:      { return "ERR"; }
     }
 
     return NULL;
@@ -14,8 +24,20 @@ void daemon_log(LogLevel logLevel, const char *format, ...) {
     va_list args;
     va_start(args, format);
 
-    printf("[DAEMON][%s] ", stringify_log_level(logLevel));
+    time_t t ;
+    struct tm *tmp ;
+    char MY_TIME[50];
+    time( &t );
+     
+    tmp = localtime( &t );
+     
+    strftime(MY_TIME, sizeof(MY_TIME), "%H:%M:%S", tmp);
+
+    printf("\x1B[90m[%s %s%s\x1B[90m] ", MY_TIME, unix_log_level_color(logLevel), stringify_log_level(logLevel));
+    
+    printf("\033[0m");
     vprintf(format, args);
+
     printf("\n");
 
     va_end(args);
--- daemon/logger.h
+++ daemon/logger.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <time.h> 
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define KGRA  "\x1B[90m"
 
 typedef enum LogLevel {
     INFO,
@@ -10,7 +22,8 @@ typedef enum LogLevel {
     ERROR
 } LogLevel;
 
-char* stringify_log_level(LogLevel logLevel);
+const char* unix_log_level_color(LogLevel logLevel);
+const char* stringify_log_level(LogLevel logLevel);
 
 void daemon_log(LogLevel logLevel, const char *format, ...);
 
