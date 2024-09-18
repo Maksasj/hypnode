6d2b39e0c40787beb27842ef7808b0a3b0792769
Aleksandras Sukelovic <alex.shukel@gmail.com>
2024-09-18 22:52:44 +0300

    Added Main.java

---
+++ compiler/src/main/java/org/compiler/cli/Main.java
@@ -0,0 +1,21 @@
+package org.compiler.cli;
+
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "hypnode2c", mixinStandardHelpOptions = true,
+        description = "Converts Hypnode programs to C code", version = "0.1")
+public class Main implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello world!");
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+}
