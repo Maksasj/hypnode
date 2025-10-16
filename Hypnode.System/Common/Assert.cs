95176ab7c8f2d612d98b84a5ddbbe816e6c956e3
Maksasj <radioboos@gmail.com>
2025-10-16 18:32:01 +0300

    Make project to net10

---
+++ Hypnode.System/Common/Assert.cs
@@ -0,0 +1,25 @@
+﻿using Hypnode.Core;
+
+namespace Hypnode.System.IO
+{
+    public class Assert : INode
+    {
+        private IConnection<bool>? inputPort = null;
+
+        public void SetInput(string portName, IConnection<bool> connection)
+        {
+            if (portName == "IN") inputPort = connection;
+        }
+
+        public void Execute()
+        {
+            if (inputPort is null)
+                throw new InvalidOperationException("Input port is not set");
+
+            var packet = inputPort.Receive();
+
+            if (!packet)
+                throw new InvalidOperationException("Assertion failed");
+        }
+    }
+}
